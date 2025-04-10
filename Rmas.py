ed1346a969f5f2e1542fa35d8c62a7178bbb457e
Fabian <fabian21cf@gmail.com>
2025-04-10 12:19:34 -0300

    asfasd

--- Rmas.py
+++ Rmas.py
@@ -1,6 +1,10 @@
+# asdasfdasdasf
 from colorama import Fore, Back, Style
+#qwrerhjyn
 print(Fore.RED + 'some red text')
+#ejrytmjmduyrh
 print(Back.GREEN + 'and with a green background')
+#sfgr1233352
 print(Style.DIM + 'and in dim text')
 print(Style.RESET_ALL)
-print("cintya")
+print('fabian')
